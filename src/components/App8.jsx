96a72513694d06bd5e493cf18b6bcebc40c07386
nairx <nairxprv@gmail.com>
2025-03-21 14:43:41 +0530

    Updated

--- src/components/App8.jsx
+++ src/components/App8.jsx
@@ -6,7 +6,6 @@ export default function App8() {
   const [total, setTotal] = useState(0);
   const handleAdd = () => {
     setAmount([...amount, value]);
-    setValue(0);
   };
   useEffect(() => {
     setTotal(
