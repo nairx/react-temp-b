65085f3037abb61ecca54af10a4cd1b0f6fa55b2
nairx <nairxprv@gmail.com>
2025-03-26 11:46:55 +0530

    Updated

--- src/components/App10.jsx
+++ src/components/App10.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useRef } from "react";
 import "./App10.css";
 export default function App10() {
   const [run, setRun] = useState(0);
   const [wicket, setWicket] = useState(0);
   const [msg, setMsg] = useState("Let's begin the game!");
-
+  const runRef = useRef();
+  const wicketRef = useRef();
+  const msgRef = useRef()
   const handleRun = () => {
     if (wicket < 10) {
       setRun(run + 1);
       setMsg("Well Done!");
+      runRef.current.style.backgroundColor = "yellow";
+      wicketRef.current.style.backgroundColor = "white";
     }
   };
 
@@ -17,26 +21,31 @@ export default function App10() {
     wicket < 10 && setWicket(wicket + 1);
     if (wicket < 9) {
       setMsg("Better luck next time!");
+      runRef.current.style.backgroundColor = "white";
+      wicketRef.current.style.backgroundColor = "yellow";
     } else {
       setMsg("Game Over");
+      msgRef.current.style.backgroundColor = "yellow"
+      runRef.current.style.backgroundColor = "white";
+      wicketRef.current.style.backgroundColor = "white";
     }
   };
   return (
     <div className="App-App10-Container">
       <h3>This is App10</h3>
       <div className="App-App10-Row">
-        <div className="App-App10-Box">
+        <div className="App-App10-Box" ref={runRef}>
           <button onClick={handleRun}>Run</button>
           <p>{run}</p>
         </div>
-        <div className="App-App10-Box">
+        <div className="App-App10-Box" ref={wicketRef}>
           <button onClick={handleWicket}>Wicket</button>
           <p>{wicket}</p>
         </div>
       </div>
       <hr></hr>
       <div className="App-App10-Row">
-        <div className="App-App10-Box"> {msg}</div>
+        <div className="App-App10-Box" ref={msgRef}> {msg}</div>
       </div>
     </div>
   );
