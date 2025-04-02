67eb1913c68d769568ddcb63ef1bca9e401623a2
nairx <nairxprv@gmail.com>
2025-04-02 15:02:11 +0530

    Updated

---
+++ src/components/App17.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Child from './Child'
+export default function App17() {
+  return (
+    <div>
+        <h3>This is App17</h3>
+        <Child num={5}/>
+    </div>
+  )
+}
