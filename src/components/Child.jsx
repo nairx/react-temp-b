4b4d57889aab3dfc5bfece40c5a6a17b08b14c7e
nairx <nairxprv@gmail.com>
2025-04-02 15:23:37 +0530

    Updated

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
-export default function Child(props) {
+import React from "react";
+import { memo } from "react";
+function Child(props) {
+  console.log("Child Component Loaded...");
   return (
-    <div>
-        {props.num}
-    </div>
-  )
+    <div>{props.num}</div>
+  );
 }
+export default memo(Child)
